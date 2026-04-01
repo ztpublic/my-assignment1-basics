905cb6eceb3f39d07704482c8f7b18da0bf19c7d
zt <zt11061@gmail.com>
2026-04-01 20:38:11 +0800

    attention

--- cs336_basics/attention.py
+++ cs336_basics/attention.py
@@ -2,6 +2,8 @@ from jaxtyping import Float, Bool
 from torch import Tensor
 import math
 import torch
+
+from cs336_basics.softmax import softmax
 def scaled_dot_product_attention(
     Q: Float[Tensor, " ... queries d_k"],
     K: Float[Tensor, " ... keys d_k"],
@@ -10,6 +12,10 @@ def scaled_dot_product_attention(
 ) -> Float[Tensor, " ... queries d_v"]:
     K_T = K.transpose(-1, -2)
     d_k = Q.size(-1)
-    seq_len = Q.size(-2)
     before_softmax = (Q @ K_T) / math.sqrt(d_k)
-    raise NotImplementedError
+    if mask is not None:
+        mask2 = torch.where(mask, 0, float('-inf'))
+        before_softmax = before_softmax + mask2
+    soft = softmax(before_softmax, -1)
+    out = soft @ V
+    return out
