9fa22b160b75cdecb998747d0629602f6f39e184
zt <zt11061@gmail.com>
2026-04-05 15:44:46 +0800

    learning rate explode test

---
+++ scripts/sdg_learning_rate_experiment.py
@@ -0,0 +1,37 @@
+from collections.abc import Callable, Iterable
+from typing import Optional
+import torch
+import math
+class SGD(torch.optim.Optimizer):
+    def __init__(self, params, lr=1e-3):
+        if lr < 0:
+             ValueError(f"Invalid learning rate: {lr}")
+        defaults = {"lr": lr}
+        super().__init__(params, defaults)
+
+    def step(self, closure: Optional[Callable] = None):
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            lr = group["lr"] # Get the learning rate.
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                state = self.state[p] # Get state associated with p.
+                t = state.get("t", 0) # Get iteration number from the state, or initial value.
+                grad = p.grad.data # Get the gradient of loss with respect to p.
+                p.data -= lr / math.sqrt(t + 1) * grad # Update weight tensor in-place.
+                state["t"] = t + 1 # Increment iteration number.
+        return loss
+    
+def main():
+    weights = torch.nn.Parameter(5 * torch.randn((10, 10)))
+    opt = SGD([weights], lr=1e3)
+    for t in range(10):
+        opt.zero_grad() # Reset the gradients for all learnable parameters.
+        loss = (weights**2).mean() # Compute a scalar loss value.
+        print(loss.cpu().item())
+        loss.backward() # Run backward pass, which computes gradients.
+        opt.step() # Run optimizer step.
+
+if __name__ == "__main__":
+    main()
