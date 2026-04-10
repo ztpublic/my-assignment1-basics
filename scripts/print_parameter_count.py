34da01281d7246113f434f8ca84a50cc654c4be9
zt <zt11061@gmail.com>
2026-04-10 22:49:21 +0800

    basic training script

--- scripts/print_parameter_count.py
+++ scripts/print_parameter_count.py
@@ -9,17 +9,14 @@ from cs336_basics.transformer import TransformerLM, TransformerLMConfig
 
 def main():
     config = TransformerLMConfig(
-        vocab_size=50_257,
-        context_length=1_024,
-        num_layers=48,
-        d_model=1_600,
-        num_heads=25,
-        d_ff=6_400,
+        vocab_size=10000,
+        context_length=256,
+        num_layers=4,
+        d_model=512,
+        num_heads=16,
+        d_ff=1344,
     )
     model = TransformerLM.from_config(config, device="meta")
-    for name, parameter in model.named_parameters():
-        if parameter.requires_grad:
-            print(f"{name}: {parameter.numel():,}")
     parameter_count = sum(parameter.numel() for parameter in model.parameters() if parameter.requires_grad)
     total_memory_mb = parameter_count * 4 / (1024 ** 2)
     print(f"TransformerLM parameter count: {parameter_count:,}")
--- scripts/training.py
+++ scripts/training.py
@@ -1,17 +1,43 @@
-from cs336_basics.transformer import TransformerLMConfig
+from cs336_basics.data_loader import get_batch
+from cs336_basics.loss import cross_entropy
+from cs336_basics.optimizer import AdamW
+from cs336_basics.softmax import softmax
+from cs336_basics.transformer import TransformerLM, TransformerLMConfig
+import torch
+import numpy as np
 
 
 def main():
     config = TransformerLMConfig(
         vocab_size=10000,
-        context_length=1_024,
-        num_layers=48,
-        d_model=1_600,
-        num_heads=25,
-        d_ff=6_400,
+        context_length=256,
+        num_layers=4,
+        d_model=512,
+        num_heads=16,
+        d_ff=1344,
     )
 
-    pass
+    arr = np.load("data/tiny-stories-10000-tokenized.npy")
+
+    model = TransformerLM.from_config(config, device="mps", dtype=torch.float32)
+    optimizer = AdamW(
+        model.parameters(), 
+        lr=1e-3,
+        weight_decay=0.01,
+        betas=(0.9, 0.999),
+        eps=1e-8,
+    )
+    for i in range(100):
+        optimizer.zero_grad()
+        inputs, targets = get_batch(arr, 32, 256, "mps")
+        out = model(inputs)
+        loss = cross_entropy(out, targets)
+        loss.backward()
+        optimizer.step()
+
+    model_state = model.state_dict()
+    torch.save(model_state, "./data/model")
+
 
 if __name__ == "__main__":
     main()
